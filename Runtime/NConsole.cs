16c48e70c4f86d9cb560f80b523f1f32bb9bf71c
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2023-05-21 11:51:35 +0200

    Clear Button

--- Runtime/NConsole.cs
+++ Runtime/NConsole.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
-using TMPro;
 
 namespace Nazio_LT.Tools.Console
 {
@@ -10,6 +10,7 @@ namespace Nazio_LT.Tools.Console
 
         [SerializeField] private Transform m_consoleContentParent = null;
         [SerializeField] private NLog m_textPrefab = null;
+        [SerializeField] private Button m_clearButton = null;
 
         private List<NLog> m_messages = new List<NLog>();
 
@@ -17,17 +18,31 @@ namespace Nazio_LT.Tools.Console
         {
             if (s_instance != null)
             {
-                HandleLogs("Another NConsole instance exist!", "", LogType.Warning);
+                HandleLog("Another NConsole instance exist!", "", LogType.Warning);
                 Destroy(gameObject);
                 return;
             }
 
             s_instance = this;
 
-            HandleLogs("NConsole initialization succed!", "", LogType.Log);
+            m_clearButton.onClick.AddListener(Clear);
+
+            HandleLog("NConsole initialization succed!", "", LogType.Log);
         }
 
-        private void HandleLogs(string condition, string stackTrace, LogType logType)
+        private void Clear()
+        {
+            if(m_messages == null || m_messages.Count == 0) return;
+
+            for (int i = 0; i < m_messages.Count; i++)
+            {
+                Destroy(m_messages[i].gameObject);
+            }
+
+            m_messages.Clear();
+        }
+
+        private void HandleLog(string condition, string stackTrace, LogType logType)
         {
             NLog log = Instantiate(m_textPrefab, transform.position, Quaternion.identity, m_consoleContentParent);
 
@@ -39,12 +54,12 @@ namespace Nazio_LT.Tools.Console
 
         private void OnEnable()
         {
-            Application.logMessageReceived += HandleLogs;
+            Application.logMessageReceived += HandleLog;
         }
 
         private void OnDisable()
         {
-            Application.logMessageReceived -= HandleLogs;
+            Application.logMessageReceived -= HandleLog;
         }
 
         public static NConsole Instance => s_instance;
